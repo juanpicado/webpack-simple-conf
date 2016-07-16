e748c02a13b8554ea5696a6a3fb8b200c11bdfce
Juan Carlos Picado <juan@encuestame.org>
2016-07-16 09:16:02 +0200

    removed filename property

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,23 @@
 const webpack = require('webpack');
 const path = require('path');
-const fs = require('fs');
 
 module.exports = function(entryPoint, outputName, library, libraryTarget) {
-
     // if it doesn"t exist then we use dist as base folder
-    if (!fs.existsSync(outputName) && !outputName.startsWith('.')) {
-        outputName = path.join(path.normalize('dist'), outputName);
+    if (!outputName.startsWith('.') || !outputName.startsWith('./')) {
+        outputName = path.normalize(path.join('dist', outputName));
+    } else {
+        outputName = path.normalize(outputName);
     }
-    console.log('outputName', outputName);
     return {
         entry: entryPoint || './lib/index.js',
         output: {
-            path: outputName,
             filename:  outputName || 'index.js',
-
             // Export itself to a global var
             libraryTarget: libraryTarget || 'var',
 
             // Name of the global var: "Foo"
             library: library
         },
-
         plugins: [
             new webpack.optimize.UglifyJsPlugin({
                 compress: {
